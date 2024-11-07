1436e8928d0cf7203222660b7ce89a1126575cca
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-11-07 08:13:35 +0600

    last touch

--- src/Components/GadgetCards.jsx
+++ src/Components/GadgetCards.jsx
@@ -3,26 +3,28 @@ import Card from "../Components/Card";
 import { useEffect, useState } from "react";
 
 const GadgetCards = () => {
-  const gadget = useLoaderData() || [];
+  const gadget = useLoaderData();
   const { category } = useParams();
   const [gadgetitem, setGadgetitem] = useState([]);
-  console.log(gadgetitem);
 
   useEffect(() => {
     if (category) {
-      const filterdBtCategory = [...gadget].filter(
+      const filteredByCategory = [...gadget].filter(
         (gadget) => gadget.category === category
       );
-      setGadgetitem(filterdBtCategory);
+      setGadgetitem(filteredByCategory);
     } else {
       setGadgetitem(gadget);
     }
   }, [category, gadget]);
+
   return (
     <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-      {gadgetitem.map((gadgets) => (
-        <Card key={gadgets.id} gadgets={gadgets}></Card>
-      ))}
+      {Array.isArray(gadgetitem) && gadgetitem.length > 0 ? (
+        gadgetitem.map((gadgets) => <Card key={gadgets.id} gadgets={gadgets} />)
+      ) : (
+        <p>No gadgets available.</p>
+      )}
     </div>
   );
 };
