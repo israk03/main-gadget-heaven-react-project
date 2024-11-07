d55412d089810293c809ac26f0d126ec45162cbd
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-11-07 06:13:52 +0600

    done

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -2,23 +2,25 @@ import { NavLink } from "react-router-dom";
 import "../App.css";
 
 const Categories = ({ categories = [] }) => {
-  if (!Array.isArray(categories)) {
-    // console.error("Expected 'categories' to be an array, but got:", categories);
-    return null;
-  }
+  console.log("Categories component received:", categories);
+
   return (
     <div>
-      {categories.map((category) => (
-        <NavLink
-          to={`/category/${category.category}`}
-          key={category.id}
-          className="bg-gray-200 py-3 rounded-full"
-        >
-          <button className="py-4 px-6 w-full active">
-            {category.category}
-          </button>
-        </NavLink>
-      ))}
+      {Array.isArray(categories) ? (
+        categories.map((category) => (
+          <NavLink
+            to={`/category/${category.category}`}
+            key={category.id}
+            className="bg-gray-200 py-3 rounded-full"
+          >
+            <button className="py-4 px-6 w-full active">
+              {category.category}
+            </button>
+          </NavLink>
+        ))
+      ) : (
+        <p>No categories available.</p>
+      )}
     </div>
   );
 };
